ac21e8103fe8894a002b14e3029a8fbf2674edcd
Mamdouh <145754753+Mamdouh-Helmy@users.noreply.github.com>
2024-03-08 14:59:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let turn = 'x';
 let title = document.querySelector(".title");
-let squares = [];
+let arr = [];
 
 function game(id){
     let squares = document.getElementById(id);
@@ -14,13 +14,28 @@ function game(id){
         title.innerHTML = 'X 🤗🤗';
     }
     winner();
+    // gameOver ();
 }
 
 function end(num1 , num2 , num3){
-        title.innerHTML = `${squares[num1]} Winner`;
-        document.getElementById("item" + num1).style.backgroundColor = '#000'
-        document.getElementById("item" + num2).style.backgroundColor = '#000'
-        document.getElementById("item" + num3).style.backgroundColor = '#000'
+        title.innerHTML = `${arr[num1]} Winner`;
+        document.getElementById("item" + num1).style.backgroundColor = '#000';
+        document.getElementById("item" + num2).style.backgroundColor = '#000';
+        document.getElementById("item" + num3).style.backgroundColor = '#000';
+        
+        setInterval( () => {
+            title.innerHTML += `.`;
+        } , 1000)
+
+        setTimeout( () => {
+            location.reload();
+        } , 3000)
+
+        turn = '';
+}
+
+function gameOver () {
+    title.innerHTML = `Game Over`;
         
         setInterval( () => {
             title.innerHTML += `.`;
@@ -35,121 +50,35 @@ function end(num1 , num2 , num3){
 
 function winner(){
     for(let i = 1 ; i < 10 ; i++){
-        squares[i] = document.getElementById("item" + i).innerHTML;
+        arr[i] = document.getElementById("item" + i).innerHTML;
     }
-    if(squares[1] === squares[2] && squares[2] === squares[3] && squares[1] !== ''){
+    if(arr[1] === arr[2] && arr[2] === arr[3] && arr[1] !== ''){
         end(1 , 2 , 3);
-    }else if (squares[4] === squares[5] && squares[5] === squares[6] && squares[4] !== '') {
+    }else if (arr[4] === arr[5] && arr[5] === arr[6] && arr[4] !== '') {
         end(4 , 5 , 6);
-    }else if (squares[7] === squares[8] && squares[8] === squares[9] && squares[7] !== '') {
+    }else if (arr[7] === arr[8] && arr[8] === arr[9] && arr[7] !== '') {
         end(7 , 8 , 9);
-    }else if (squares[1] === squares[5] && squares[5] === squares[9] && squares[1] !== '') {
+    }else if (arr[1] === arr[5] && arr[5] === arr[9] && arr[1] !== '') {
         end(1 , 5 , 9);
-    }else if (squares[3] === squares[5] && squares[5] === squares[7] && squares[3] !== '') {
+    }else if (arr[3] === arr[5] && arr[5] === arr[7] && arr[3] !== '') {
         end(3 , 5 , 7);
-    }else if (squares[1] === squares[4] && squares[4] === squares[7] && squares[1] !== '') {
+    }else if (arr[1] === arr[4] && arr[4] === arr[7] && arr[1] !== '') {
         end(1 , 4 , 7);
-    }else if (squares[2] === squares[5] && squares[5] === squares[8] && squares[2] !== '') {
+    }else if (arr[2] === arr[5] && arr[5] === arr[8] && arr[2] !== '') {
         end(2 , 5 , 8);
-    }else if (squares[3] === squares[6] && squares[6] === squares[9] && squares[3] !== '') {
+    }else if (arr[3] === arr[6] && arr[6] === arr[9] && arr[3] !== '') {
         end(3 , 6 , 9);
+    }else if (arr.every(cell => cell !== '')) {
+        title.innerHTML = `Game Over`;
+        
+        setInterval(() => {
+            title.innerHTML += `.`;
+        }, 1000);
+    
+        setTimeout(() => {
+            location.reload();
+        }, 3000);
+    
+        turn = '';
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let turn = 'x';
-// let title = document.querySelector(".title");
-// let squares = [];
-
-// function game(id){
-//     let elemnt = document.getElementById(id);
-//     if(turn === 'x' && elemnt.innerHTML === ''){
-//         elemnt.innerHTML = 'x';
-//         turn = 'o'
-//         title.innerHTML = 'o';
-//     }else if (turn === 'o' && elemnt.innerHTML === '') {
-//         elemnt.innerHTML = 'o';
-//         turn = 'x'
-//         title.innerHTML = 'x';
-//     }
-//     winner();
-// }
-
-// function end(num1 , num2 , num3){
-//     title.innerHTML = `${squares[num1]} winner`;
-//         document.getElementById("item"+num1).style.backgroundColor = '#000';
-//         document.getElementById("item"+num2).style.backgroundColor = '#000';
-//         document.getElementById("item"+num3).style.backgroundColor = '#000';
-
-//         setInterval(() => {
-//             title.innerHTML += '.'
-//         }, 1000);
-//         setTimeout( () => {
-//             location.reload();
-//         } , 4000)
-// }
-
-// function winner(){
-//     for( let i = 1 ; i< 10 ; i++){
-//         squares[i] = document.getElementById("item" + i).innerHTML;
-//     }
-//     if(squares[1] === squares[2] && squares[2] === squares[3] && squares[1] != ''){
-//         end(1,2,3);
-//     }else if (squares[4] === squares[5] && squares[5] === squares[6] && squares[4] != '') {
-//         end(4,5,6);
-//     }else if (squares[7] === squares[8] && squares[8] === squares[9] && squares[7] != '') {
-//         end(7,8,9);
-//     }else if (squares[1] === squares[5] && squares[5] === squares[9] && squares[1] != '') {
-//         end(1,5,9);
-//     }else if (squares[3] === squares[5] && squares[5] === squares[7] && squares[3] != '') {
-//         end(3,5,7);
-//     }
-// }
